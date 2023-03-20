46cad7be98dfd09edd51ffb316272ea49a6a9a1a
peter-swe <lelkes@me.com>
2023-03-20 19:06:42 +0100

    first try with git/github

---
+++ screens/AllExpenses.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+
+const AllExpenses = () => {
+  return (
+    <View>
+      <Text>All Expenses Screen</Text>
+    </View>
+  );
+};
+
+export default AllExpenses;
+
+const styles = StyleSheet.create({});
---
+++ screens/ManageExpense.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+
+const ManageExpense = () => {
+  return (
+    <View>
+      <Text>Manage Expense Screen</Text>
+    </View>
+  );
+};
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({});
